c6fe6f21b2a74dc5914fe5cdf54084b9f6e00ac7
Axl Enrique Diaz Hernandez <78901794+Solid876@users.noreply.github.com>
2024-10-12 17:24:43 -0600

    Add files via upload

---
+++ Parcial 2/dragonball.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `dragonball`
+ */
+
+/* `dragonball`.`characters` */
+$characters = array(
+  array('id' => '1','user_id' => '1','name' => 'Changuito','race' => 'Saiyan','level' => '1','strength' => '50','speed' => '50','endurance' => '50','experience' => '0','avatar' => NULL)
+);
+
+/* `dragonball`.`character_transformations` */
+$character_transformations = array(
+);
+
+/* `dragonball`.`tournaments` */
+$tournaments = array(
+);
+
+/* `dragonball`.`tournament_participants` */
+$tournament_participants = array(
+);
+
+/* `dragonball`.`transformations` */
+$transformations = array(
+);
+
+/* `dragonball`.`users` */
+$users = array(
+  array('id' => '1','name' => 'Axl Enrique Diaz Hernandez','email' => 'axlenriquediazh22.ce84@dgeti.sems.gob.mx','password' => 'Hola12345','avatar' => ''),
+  array('id' => '2','name' => 'Pedro Ramirez Juarez','email' => 'peter777@gmail.com','password' => 'Pedro9999','avatar' => '')
+);
